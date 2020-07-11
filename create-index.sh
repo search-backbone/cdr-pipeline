16c101ef3dbd1ca6fcc1d64e9b7ea276d28b491f
tansudasli <tansudasli@gmail.com>
2020-07-11 11:14:24 +0300

    minor update

--- create-index.sh
+++ create-index.sh
@@ -4,3 +4,6 @@ source .elk.env
 curl -X PUT "${ELASTIC_IP}:9200/${INDEX_NAME}?pretty" \
      -H 'Content-Type: application/json' \
      --data-binary '@mappings/call_records_mapping.json'
+
+# check
+curl "localhost:9200/call-records?pretty"
